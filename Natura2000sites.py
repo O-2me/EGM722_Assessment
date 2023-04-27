c04041b52b120ef147ca4fc331455f4f2ad4826b
O-2me <129533730+O-2me@users.noreply.github.com>
2023-04-27 16:50:43 +0100

    EPSG loop checker/converter function and userpoint input added

--- Natura2000sites.py
+++ Natura2000sites.py
@@ -4,6 +4,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
+from shapely.geometry import Point, LineString
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 
@@ -33,29 +34,51 @@ def scale_bar(ax, location=(0.92, 0.05)):
     ax.text(sbx-12500, sby-10000, '10 km', transform=ax.projection, fontsize=8)
     ax.text(sbx-24500, sby-10000, '0 km', transform=ax.projection, fontsize=8)
 
-# import external datasets
-# load the Republic of Ireland boundary as an outline
+# ---------------------Import external shapefiles as GeoPandas Geodataframes----------------------------------------
 outline = gpd.read_file(os.path.abspath('data_files/Counties___Ungen_2019.shp'))
-
-# load Natura 2000 Site boundaries
 sac = gpd.read_file(os.path.abspath('data_files/SAC_ITM_2023_02.shp'))
 spa = gpd.read_file(os.path.abspath('data_files/SPA_ITM_2021_10.shp'))
+#---------------------------------Check CRS Consistency--------------------------------------------------------------
+#sac = sac.to_crs(epsg=32639) #debug
+
+#Check EPSG codes of the input data and state if projections are consistent
+if outline.crs == sac.crs == spa.crs:
+    print ('All features are projected to projection {}'.format(outline.crs))
+    pass
+else:
+    print ('Input data will be reprojected as 1 or more features have inconsistent projections')
+    epsg_code = int((input('Please enter the EPSG code used to reproject input data: '))) #ITM = 2157
+    outline = outline.to_crs(epsg=epsg_code)
+    sac = sac.to_crs(epsg=epsg_code)
+    spa = spa.to_crs(epsg=epsg_code)
+    print('Outline projection - {}\nSAC projection - {}\nSPA projection - {}'
+          .format(outline.crs, sac.crs, spa.crs))
+
+#------------------------------User input coordinates and search function-------------------------------------------
+
+xin = input("Enter X coordinates of search point:")
+xin = float(xin) #transform input str to float for plotting
+
+yin = input('Entey Y coodinate of search point;')
+yin = float(yin) #transform input str to float for plotting
+
+userinput = (xin, yin)
+
 
 #_______________________________Creating map________________________________________________________________________
 # create figure of size 10x10
 myFig = plt.figure(figsize=(10, 10))
 
-# create a Universal Transverse Mercator reference system to transform data.
-myCRS = ccrs.UTM(29)  # RoI is in UTM Zone 29, so we pass 29 to ccrs.UTM()
+# create a coordinate reference system.
+myCRS = ccrs.UTM(29)  # Irish Transverse Mercator (ITM) is used in RoI.
+# 2157 is the epsg code for ITM so passed to ccrs.epsg()
 
 # create an axis object in the figure, using myCRS where data is plotted.
 ax = plt.axes(projection=myCRS)
 
 # add the outline of RoI using cartopy's ShapelyFeature
 outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
-
-
-ax.add_feature(outline_feature)  # add the features we've created to the map.
+ax.add_feature(outline_feature)
 
 # using the boundary of the shapefile features, zoom the map to our area of interest
 xmin, ymin, xmax, ymax = outline.total_bounds
@@ -80,23 +103,25 @@ spa_feat = ShapelyFeature(spa['geometry'],  # first argument is the geometry
                             alpha= 0.5)    # Transparency set to 50%
 ax.add_feature(spa_feat)  # add the feature to the map
 
+userpoint_handle = ax.plot(xin, yin, 'o', color='r', ms=6,)
+
 # Symbologies for handles in legend
 sac_handle = generate_handles(['SAC'], ['g'])
 spa_handle = generate_handles(['SPA'], ['b'])
 
 # ax.legend() takes list of handles and list of labels corresponding to the features of interest
 # and adds them to  the legend
-handles = sac_handle + spa_handle
-labels = ['SAC', 'SPA']
+handles = sac_handle + spa_handle + userpoint_handle
+labels = ['SAC', 'SPA', 'Search Point']
 leg = ax.legend(handles, labels, title='Legend', title_fontsize=12,
                 fontsize=10, loc='upper left', frameon=True, framealpha=1)
 
 # Gridlines - I'll probably remove
-gridlines = ax.gridlines(draw_labels=True,  # draw  labels for the grid lines
-                         xlocs=[-9.5, -9, -8.5, -8, -7.5, -7],  # add longitude lines at 0.5 deg intervals
-                         ylocs=[54, 54.5, 55, 55.5])  # add latitude lines at 0.5 deg intervals
-gridlines.left_labels = False  # turn off the left-side labels
-gridlines.bottom_labels = False  # turn off the bottom labels
+#gridlines = ax.gridlines(draw_labels=True,  # draw  labels for the grid lines
+#                         xlocs=[-9.5, -9, -8.5, -8, -7.5, -7],  # add longitude lines at 0.5 deg intervals
+#                         ylocs=[54, 54.5, 55, 55.5])  # add latitude lines at 0.5 deg intervals
+#gridlines.left_labels = False  # turn off the left-side labels
+#gridlines.bottom_labels = False  # turn off the bottom labels
 
 # add the scale bar to the axis
 scale_bar(ax)
